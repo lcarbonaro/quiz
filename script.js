8b0e7ecbbde440b76e5a6a2f0774ae583b6c1351
Les Carbonaro <lcarbonaro@gmail.com>
2019-07-03 19:34:29 -0400

    updates

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
    
     let quiz = await getQuiz();
 
-    /*
+    /* testing
     let quiz = [
         {question:"Q#1", answer:"A#1"},
         {question:"Q#2", answer:"A#2"},
         {question:"Q#3", answer:"A#3"}
     ];
-    */
+    */    
 
     let current = -1;
 
@@ -19,8 +19,6 @@
     let front = document.querySelector('.front > p');
     let back = document.querySelector('.back > p');
 
-    nextCard();
-
     async function getQuiz() {
         let resp = await fetch('https://us-central1-quiz-eef08.cloudfunctions.net/getData');
         let respJson = await resp.json();
@@ -30,8 +28,7 @@
     function nextCard() {
         document.querySelector('div.card').classList.remove('flipped');
         setTimeout(()=>{
-            current = (current + 1) % quiz.length;
-            //console.log(quiz[current]);
+            current = (current + 1) % quiz.length;            
             front.textContent = quiz[current].question;
             back.textContent = quiz[current].answer;
         }, 500);        
@@ -40,8 +37,7 @@
     function prevCard() {
         document.querySelector('div.card').classList.remove('flipped');
         setTimeout(()=>{
-            current = Math.abs(current - 1) % quiz.length;
-            //console.log(quiz[current]);
+            current = Math.abs(current - 1) % quiz.length;            
             front.textContent = quiz[current].question;
             back.textContent = quiz[current].answer;
         }, 500);        
