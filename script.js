f472e9305008f5841b781b10f4f8937d793997c9
Les Carbonaro <lcarbonaro@gmail.com>
2019-07-04 18:48:44 -0400

    updates

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 (async function () {
+
+    let front = document.querySelector('.front > p');
+    let back  = document.querySelector('.back > p');
+    let card  = document.querySelector('div.card');
+
+    document.querySelector('#btnPrev').addEventListener('click', prevCard);
+    document.querySelector('#btnFlip').addEventListener('click', flipCard);
+    document.querySelector('#btnNext').addEventListener('click', nextCard);
    
     let quiz = await getQuiz();
 
-    /* testing
+    /* test data
     let quiz = [
         {question:"Q#1", answer:"A#1"},
         {question:"Q#2", answer:"A#2"},
         {question:"Q#3", answer:"A#3"}
     ];
-    */    
+    */
 
     let current = -1;
-
-    document.querySelector('#btnPrev').addEventListener('click', prevCard);
-    document.querySelector('#btnFlip').addEventListener('click', flipCard);
-    document.querySelector('#btnNext').addEventListener('click', nextCard);
-
-    let front = document.querySelector('.front > p');
-    let back = document.querySelector('.back > p');
 
     async function getQuiz() {
         let resp = await fetch('https://us-central1-quiz-eef08.cloudfunctions.net/getData');
@@ -26,7 +27,7 @@
     }    
 
     function nextCard() {
-        document.querySelector('div.card').classList.remove('flipped');
+        card.classList.remove('flipped');
         setTimeout(()=>{
             current = (current + 1) % quiz.length;            
             front.textContent = quiz[current].question;
@@ -35,7 +36,7 @@
     }
 
     function prevCard() {
-        document.querySelector('div.card').classList.remove('flipped');
+        card.classList.remove('flipped');
         setTimeout(()=>{
             current = Math.abs(current - 1) % quiz.length;            
             front.textContent = quiz[current].question;
@@ -44,7 +45,7 @@
     }
     
     function flipCard() {
-        document.querySelector('div.card').classList.toggle('flipped');
+        card.classList.toggle('flipped');
     }
 
 })();
