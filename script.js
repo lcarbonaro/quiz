47ddb07130de1916f282f104622f6666eaca654c
Les Carbonaro <lcarbonaro@gmail.com>
2019-07-27 11:21:25 -0400

    updates

--- script.js
+++ script.js
@@ -10,16 +10,17 @@
    
     let quiz = await getQuiz();
 
-    /* test data
+    /* test data */
+    /*
     let quiz = [
         {question:"Q#1", answer:"A#1"},
         {question:"Q#2", answer:"A#2"},
         {question:"Q#3", answer:"A#3"}
     ];
-    */
+    */    
 
     let current = -1;
-
+    
     async function getQuiz() {
         let resp = await fetch('https://us-central1-quiz-eef08.cloudfunctions.net/getData');
         let respJson = await resp.json();
@@ -38,7 +39,7 @@
     function prevCard() {
         card.classList.remove('flipped');
         setTimeout(()=>{
-            current = (current - 1 < 0 ? 0 : current -1) % quiz.length;            
+            current = Math.abs(current - 1) % quiz.length;            
             front.textContent = quiz[current].question;
             back.textContent = quiz[current].answer;
         }, 500);        
